f720ca12ffa10790bbce86eb9694a71f9275f5b5
Kanishk Vishwakarma <kanishk.vishwa2001@gmail.com>
2021-09-16 14:54:32 +0530

    Furnished code for single straight line

--- straight_planner/src/straight_planner.cpp
+++ straight_planner/src/straight_planner.cpp
@@ -1,90 +1,39 @@
-/* iPath: A C++ Library of Intelligent Global Path Planners for Mobile Robots with ROS Integration. 
- * Website: http://www.iroboapp.org/index.php?title=IPath
- * Contact: 
- *
- * Copyright (c) 2014
- * Owners: Al-Imam University/King AbdulAziz Center for Science and Technology (KACST)/Prince Sultan University
- * All rights reserved.
- *
- * License Type: GNU GPL
- *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h> 
-#include <fstream>
-#include <iostream>
-#include <iomanip>
-#include <string>
 
-#include "ADAstar_ros.h"
+#include "straight_planner.h"
 
 #include <pluginlib/class_list_macros.h>
 //register this planner as a BaseGlobalPlanner plugin
-PLUGINLIB_EXPORT_CLASS(ADAstar_planner::ADAstarPlannerROS, nav_core::BaseGlobalPlanner)
+PLUGINLIB_EXPORT_CLASS(straight_planner_ns::straight_planner, nav_core::BaseGlobalPlanner)
 
 
 int value;
 int mapSize;
 bool* OGM;
-static const float INFINIT_COST = INT_MAX; //!< cost of non connected nodes
-float infinity = std::numeric_limits< float >::infinity();
-float tBreak;  // coefficient for breaking ties
 
-int clock_gettime(clockid_t clk_id, struct timespect *tp);
-
-timespec diff(timespec start, timespec end)
-{
-  timespec temp;
-	if ((end.tv_nsec-start.tv_nsec)<0) {
-		temp.tv_sec = end.tv_sec-start.tv_sec-1;
-		temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
-	} else {
-		temp.tv_sec = end.tv_sec-start.tv_sec;
-		temp.tv_nsec = end.tv_nsec-start.tv_nsec;
-	}
-	return temp;
-}
 
 inline vector <int> findFreeNeighborCell (int CellID);
 
-namespace ADAstar_planner
+namespace straight_planner_ns
 {
 
 //Default Constructor
-ADAstarPlannerROS::ADAstarPlannerROS()
+straight_planner::straight_planner()
 {
 
 }
-ADAstarPlannerROS::ADAstarPlannerROS(ros::NodeHandle &nh)
+straight_planner::straight_planner(ros::NodeHandle &nh)
 {
   ROSNodeHandle = nh;
 
 }
 
-ADAstarPlannerROS::ADAstarPlannerROS(std::string name, costmap_2d::Costmap2DROS* costmap_ros)
+straight_planner::straight_planner(std::string name, costmap_2d::Costmap2DROS* costmap_ros)
 {
   initialize(name, costmap_ros);
 }
 
-void ADAstarPlannerROS::initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros)
+void straight_planner::initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros)
 {
 
   if (!initialized_)
@@ -103,7 +52,6 @@ void ADAstarPlannerROS::initialize(std::string name, costmap_2d::Costmap2DROS* c
 	height = costmap_->getSizeInCellsY();
 	resolution = costmap_->getResolution();
 	mapSize = width*height;
-	tBreak = 1+1/(mapSize); 
 	value =0;
 
 
@@ -113,7 +61,6 @@ void ADAstarPlannerROS::initialize(std::string name, costmap_2d::Costmap2DROS* c
       for (unsigned int ix = 0; ix < costmap_->getSizeInCellsX(); ix++)
       {
         unsigned int cost = static_cast<int>(costmap_->getCost(ix, iy));
-        //cout<<cost;
         if (cost == 0)
           OGM[iy*width+ix]=true;
         else
@@ -123,14 +70,14 @@ void ADAstarPlannerROS::initialize(std::string name, costmap_2d::Costmap2DROS* c
 
 
 
-    ROS_INFO("ADAstar planner initialized successfully");
+    ROS_INFO("Straight line planner initialized successfully");
     initialized_ = true;
   }
   else
     ROS_WARN("This planner has already been initialized... doing nothing");
 }
 
-bool ADAstarPlannerROS::makePlan(const geometry_msgs::PoseStamped& start, const geometry_msgs::PoseStamped& goal,
+bool straight_planner::makePlan(const geometry_msgs::PoseStamped& start, const geometry_msgs::PoseStamped& goal,
                              std::vector<geometry_msgs::PoseStamped>& plan)
 {
 
@@ -195,7 +142,7 @@ bool ADAstarPlannerROS::makePlan(const geometry_msgs::PoseStamped& start, const 
         vector<int> bestPath;
 	bestPath.clear();
 
-    bestPath = ADAstarPlanner(startCell, goalCell);
+    bestPath = planner_func(startCell, goalCell);
 
 //if the global planner find a path
     if ( bestPath.size()>0)
@@ -262,7 +209,7 @@ bool ADAstarPlannerROS::makePlan(const geometry_msgs::PoseStamped& start, const 
   }
 
 }
-void ADAstarPlannerROS::getCorrdinate(float& x, float& y)
+void straight_planner::getCorrdinate(float& x, float& y)
 {
 
   x = x - originX;
@@ -270,7 +217,7 @@ void ADAstarPlannerROS::getCorrdinate(float& x, float& y)
 
 }
 
-int ADAstarPlannerROS::convertToCellIndex(float x, float y)
+int straight_planner::convertToCellIndex(float x, float y)
 {
 
   int cellIndex;
@@ -283,7 +230,7 @@ int ADAstarPlannerROS::convertToCellIndex(float x, float y)
   return cellIndex;
 }
 
-void ADAstarPlannerROS::convertToCoordinate(int index, float& x, float& y)
+void straight_planner::convertToCoordinate(int index, float& x, float& y)
 {
 
   x = getCellColID(index) * resolution;
@@ -295,7 +242,7 @@ void ADAstarPlannerROS::convertToCoordinate(int index, float& x, float& y)
 
 }
 
-bool ADAstarPlannerROS::isCellInsideMap(float x, float y)
+bool straight_planner::isCellInsideMap(float x, float y)
 {
   bool valid = true;
 
@@ -305,37 +252,22 @@ bool ADAstarPlannerROS::isCellInsideMap(float x, float y)
   return valid;
 }
 
-void ADAstarPlannerROS::mapToWorld(double mx, double my, double& wx, double& wy){
+void straight_planner::mapToWorld(double mx, double my, double& wx, double& wy){
    costmap_2d::Costmap2D* costmap = costmap_ros_->getCostmap();
     wx = costmap->getOriginX() + mx * resolution;
     wy = costmap->getOriginY() + my * resolution;
 }
 
-vector<int> ADAstarPlannerROS::ADAstarPlanner(int startCell, int goalCell){
+vector<int> straight_planner::planner_func(int startCell, int goalCell){
 
    vector<int> bestPath;
 
 cout<<endl<<"MapSize is: "<<mapSize<<endl;
 
-//float g_score [mapSize][2];
-//float g_score [mapSize];
 
 
-for (int i=0; i<mapSize; i++)
-//	g_score[i]=infinity;
 
-   timespec time1, time2;
-  /* take current time here */
-//   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
-
-//  bestPath=findPath(startCell, goalCell,  g_score);
   bestPath=findPath(startCell, goalCell);
-
-  // clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
-
-
-//   cout<<"time to generate Straight line = " << (diff(time1,time2).tv_sec)*1e3 + (diff(time1,time2).tv_nsec)*1e-6 << " microseconds" << endl;
-   
 
   return bestPath;
 
@@ -348,8 +280,8 @@ for (int i=0; i<mapSize; i++)
 //Output: the best path
 //Description: it is used to generate the robot free path
 /*********************************************************************************/
-//vector<int> ADAstarPlannerROS::findPath(int startCell, int goalCell, float g_score[])
-vector<int> ADAstarPlannerROS::findPath(int startCell, int goalCell)
+//vector<int> straight_planner::findPath(int startCell, int goalCell, float g_score[])
+vector<int> straight_planner::findPath(int startCell, int goalCell)
 {
 	cout << "Start cell: " << startCell << " (" << getCellRowID(startCell) <<", " << getCellColID(startCell) <<")"<< ", Goal cell: " << goalCell << " (" << getCellRowID(goalCell) <<", " << getCellColID(goalCell) <<")"<< endl;
 	int start_x = getCellRowID(startCell);
@@ -438,81 +370,6 @@ vector<int> ADAstarPlannerROS::findPath(int startCell, int goalCell)
 	return bestPath;
 }
 
-/*******************************************************************************/
-//Function Name: constructPath
-//Inputs: the start and the goal Cells
-//Output: the best path
-//Description: it is used to construct the robot path
-/*********************************************************************************/
-//vector<int> ADAstarPlannerROS::constructPath(int startCell, int goalCell,float g_score[])
-//vector<int> ADAstarPlannerROS::constructPath(int startCell, int goalCell)
-//{
-//	vector<int> bestPath;
-//	vector<int> path;
-//
-//	path.insert(path.begin()+bestPath.size(), goalCell);
-//	int currentCell=goalCell;
-//
-//	while(currentCell!=startCell)
-//	{ 
-//		vector <int> neighborCells;
-//		neighborCells=findFreeNeighborCell(currentCell);
-//
-//		vector <float> gScoresNeighbors;
-//		for(uint i=0; i<neighborCells.size(); i++)
-//			gScoresNeighbors.push_back(g_score[neighborCells[i]]);
-//		
-//		int posMinGScore=distance(gScoresNeighbors.begin(), min_element(gScoresNeighbors.begin(), gScoresNeighbors.end()));
-//		currentCell=neighborCells[posMinGScore];
-//
-//		//insert the neighbor in the path
-//		path.insert(path.begin()+path.size(), currentCell);
-//	}
-//	for(uint i=0; i<path.size(); i++)
-//		bestPath.insert(bestPath.begin()+bestPath.size(), path[path.size()-(i+1)]);
-//
-//	return bestPath;
-//}
-
-/*******************************************************************************/
-//Function Name: calculateHCost
-//Inputs:the cellID and the goalCell
-//Output: the distance between the current cell and the goal cell
-//Description: it is used to calculate the hCost 
-/*********************************************************************************/
-/*
-float ADAstarPlannerROS::calculateHCost(int cellID, int goalCell)
-{    
-  int x1=getCellRowID(goalCell);
-  int y1=getCellColID(goalCell);
-  int x2=getCellRowID(cellID);
-  int y2=getCellColID(cellID);
-  
-  //if(getNeighborNumber()==4) 
-    //The diagonal shortcut distance between two grid points (x1,y1) and (x2,y2) is:
-    //  return min(abs(x1-x2),abs(y1-y2))*sqrt(2) + max(abs(x1-x2),abs(y1-y2))-min(abs(x1-x2),abs(y1-y2));
-  
-  //else
-    //manhatten distance for 8 neighbor
-    return abs(x1-x2)+abs(y1-y2);
-}
-*/
-/*******************************************************************************/
-//Function Name: addNeighborCellToOpenList
-//Inputs: the open list, the neighbors Cell, the g_score matrix, the goal cell 
-//Output: 
-//Description: it is used to add a neighbor Cell to the open list
-/*********************************************************************************/
-//void ADAstarPlannerROS::addNeighborCellToOpenList(multiset<cells> & OPL, int neighborCell, int goalCell, float g_score[])
-//{
-//	cells CP;
-//	CP.currentCell=neighborCell; //insert the neighbor cell
-//	CP.fCost=g_score[neighborCell]+calculateHCost(neighborCell,goalCell);
-//	OPL.insert(CP);
-//	//multiset<cells>::iterator it = OPL.lower_bound(CP);
-//	//multiset<cells>::iterator it = OPL.upper_bound(CP);
-//	//OPL.insert( it, CP  );
-//}
 
   /*******************************************************************************
  * Function Name: findFreeNeighborCell
@@ -522,7 +379,7 @@ float ADAstarPlannerROS::calculateHCost(int cellID, int goalCell)
  * Check Status: Checked by Anis, Imen and Sahar
 *********************************************************************************/
 
-vector <int> ADAstarPlannerROS::findFreeNeighborCell (int CellID){
+vector <int> straight_planner::findFreeNeighborCell (int CellID){
  
   int rowID=getCellRowID(CellID);
   int colID=getCellColID(CellID);
@@ -548,7 +405,7 @@ vector <int> ADAstarPlannerROS::findFreeNeighborCell (int CellID){
 //Output: true if the start and the goal cells are valid
 //Description: check if the start and goal cells are valid
 /*********************************************************************************/
-bool ADAstarPlannerROS::isStartAndGoalCellsValid(int startCell,int goalCell)
+bool straight_planner::isStartAndGoalCellsValid(int startCell,int goalCell)
 { 
  bool isvalid=true;
  bool isFreeStartCell=isFree(startCell);
@@ -602,47 +459,16 @@ bool ADAstarPlannerROS::isStartAndGoalCellsValid(int startCell,int goalCell)
 }
 
 
- float  ADAstarPlannerROS::getMoveCost(int i1, int j1, int i2, int j2){
-   float moveCost=INFINIT_COST;//start cost with maximum value. Change it to real cost of cells are connected
-   //if cell2(i2,j2) exists in the diagonal of cell1(i1,j1)
-   if((j2==j1+1 && i2==i1+1)||(i2==i1-1 && j2==j1+1) ||(i2==i1-1 && j2==j1-1)||(j2==j1-1 && i2==i1+1)){
-     //moveCost = DIAGONAL_MOVE_COST;
-     moveCost = 1.4;
-   }
-    //if cell 2(i2,j2) exists in the horizontal or vertical line with cell1(i1,j1)
-   else{
-     if ((j2==j1 && i2==i1-1)||(i2==i1 && j2==j1-1)||(i2==i1+1 && j2==j1) ||(i1==i2 && j2==j1+1)){
-       //moveCost = MOVE_COST;
-       moveCost = 1;
-     }
-   }
-   return moveCost;
- } 
- 
-  float  ADAstarPlannerROS::getMoveCost(int CellID1, int CellID2){
-   int i1=0,i2=0,j1=0,j2=0;
-    
-   i1=getCellRowID(CellID1);
-   j1=getCellColID(CellID1);
-   i2=getCellRowID(CellID2);
-   j2=getCellColID(CellID2);
-    
-    return getMoveCost(i1, j1, i2, j2);
- } 
-
-
  //verify if the cell(i,j) is free
- bool  ADAstarPlannerROS::isFree(int i, int j){
+ bool  straight_planner::isFree(int i, int j){
    int CellID = getCellIndex(i, j);
  return OGM[CellID];
 
  } 
 
   //verify if the cell(i,j) is free
- bool  ADAstarPlannerROS::isFree(int CellID){
+ bool  straight_planner::isFree(int CellID){
  return OGM[CellID];
  } 
 }
 ;
-
-bool operator<(cells const &c1, cells const &c2) { return c1.fCost < c2.fCost; }
